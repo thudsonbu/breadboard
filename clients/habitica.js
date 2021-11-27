593cca0e503ae54eea45521d73ee2c632d165b65
thudsonbu <thudson@starry.com>
2021-11-27 14:02:23 -0700

    added todoist client and refactored habitica client to seperate instance

--- clients/habitica.js
+++ clients/habitica.js
@@ -1,9 +1,13 @@
 const axios          = require('axios');
 const habiticaConfig = require('../private/habitica_config');
 
-axios.defaults.baseURL = 'http://habitica.com/api/v3';
-axios.defaults.headers.common['x-api-user'] = habiticaConfig.USER_ID;
-axios.defaults.headers.common['x-api-key'] = habiticaConfig.API_KEY;
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+const client = axios.create({
+  baseURL: 'http://habitica.com/api/v3',
+  headers: {
+    'x-api-user': habiticaConfig.USER_ID,
+    'x-api-key': habiticaConfig.API_KEY,
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+})
 
-module.exports = axios;
+module.exports = client;
---
+++ clients/todoist.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+const todoistConfig = require('../private/todoist_config');
+
+const client = axios.create({
+  baseURL: 'https://api.todoist.com/rest/v1',
+  headers: {
+    'Authorization': 'Bearer ' + todoistConfig.API_TOKEN,
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+});
+
+module.exports = client;
