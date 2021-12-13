b595d32e5e5d89ae708b97adb9d736ad8f1d574a
thudsonbu <thudson@starry.com>
2021-12-12 19:35:48 -0700

    wp

--- test.js
+++ test.js
@@ -2,10 +2,14 @@ const habitica = require('./lib/resources/habitica');
 const logger   = require('@starryinternet/jobi');
 
 async function main() {
-  const res = await habitica.createTask({
-    text: 'new task',
-    type: 'todo',
-  });
+  try {
+    const res = await habitica.createTask({
+      text: 'new task',
+      type: 'todo',
+    });
+  } catch (err) {
+    logger.error( err.message )
+  }
 }
 
 main();
