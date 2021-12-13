00e8fdfeba1d4d241d877d741fc6cbc0d6d64317
thudsonbu <thudson@starry.com>
2021-12-12 20:28:52 -0700

    habitica resource methods

--- test.js
+++ test.js
@@ -2,14 +2,7 @@ const habitica = require('./lib/resources/habitica');
 const logger   = require('@starryinternet/jobi');
 
 async function main() {
-  try {
-    const res = await habitica.createTask({
-      text: 'new task',
-      type: 'todo',
-    });
-  } catch (err) {
-    logger.error( err.response )
-  }
+  habitica.relayTask({ })
 }
 
 main();
