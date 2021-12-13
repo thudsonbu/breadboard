4aca6a937b558d175214173a407d445b880e1562
thudsonbu <thudson@starry.com>
2021-12-12 21:57:20 -0700

    basic app test

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-const Hapi     = require('@hapi/hapi');
-const logger   = require('@starryinternet/jobi');
-const habitica = require('./lib/resources/habitica');
+const app = require('./lib/app');
 
-const init = async() => {
-  const port = process.env.PORT || 3000;
-
-  const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
-  });
-
-  server.route({
-    method: 'POST',
-    path: '/habitica/relay',
-    handler: async ( req, h ) => {
-      logger.debug( 'request to /habitica/relay' );
-
-      await habitica.relayItem({
-        text: 'todoist text',
-        difficulty: 'todoist difficulty'
-      });
-    }
-  });
-
-  await server.start();
-
-  logger.info('hapi server started on port ' + port);
-}
-
-init();
+app();
---
+++ test/tests/app.js
@@ -0,0 +1,8 @@
+const app = require('../../lib/app');
+
+describe( 'lib/app', () => {
+  it( 'should start and stop', async() => {
+    const instance = await app();
+    await instance.stop();
+  });
+});
