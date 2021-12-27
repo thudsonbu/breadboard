b5a5b98a8403efc0e9462aa5ec24005fa7a4bb3a
thudsonbu <thudson@starry.com>
2021-12-26 19:48:11 -0700

    modify habitical singleton based on env

--- test/tests/app.js
+++ test/tests/app.js
@@ -1,8 +1,10 @@
 const app = require('../../lib/app');
 
 describe( 'lib/app', () => {
+
   it( 'should start and stop', async() => {
     const instance = await app();
     await instance.stop();
   });
+
 });
