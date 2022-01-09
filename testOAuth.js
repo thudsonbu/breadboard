60fd2a0ad251f86cd284988b836b907cc6ca6fd5
thudsonbu <thudson@starry.com>
2022-01-08 18:36:28 -0700

    fixup! Added todoist class, added token url gen script, added todoist class, updated naming conventions

--- testOAuth.js
+++
@@ -1,8 +0,0 @@
-const todoistClients = require('./lib/clients/todoist');
-
-const clientId = '7cf4a927c4cd434687cf62b968d90dc5';
-const clientSecret = '95717dedcd23468f9c8852262b2e8420';
-
-todoistClients.oAuth( clientId, clientSecret ).then( ( res ) => {
-  console.log( res );
-});
