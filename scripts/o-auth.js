1ca962f663a0a41e7a6d6cee3bc5a4abec9c8ec5
thudsonbu <54583311+thudsonbu@users.noreply.github.com>
2022-10-21 13:48:05 -0400

    feat(scripts): add script for easily completing OAuth flow.

--- scripts/o-auth.js
+++ scripts/o-auth.js
@@ -1,23 +1,82 @@
-const ConsoleIO = require('./consoleIO');
-const crypto    = require('crypto');
+const ConsoleIO = require("./consoleIO");
+const crypto = require("crypto");
+const axios = require("axios");
+const Hapi = require("@hapi/hapi");
+const assert = require("assert");
 
 /**
- * Perform OAuth flow for sync api to retrieve an api Token
- * @param {string} clientId - todoist application client id
- * @param {string} clientSecret - todoist application client secret
- * @returns {Promise<string>} - todoist sync api token
+ * Perform OAuth flow and get an api token
  */
 async function oAuth() {
   const consoleIO = new ConsoleIO();
+  const secretString = crypto.randomBytes(64).toString("hex");
 
-  const secretString = crypto.randomBytes( 64 ).toString('hex');
+  let clientId;
+  let clientSecret;
 
-  const clientId     = await consoleIO.query('Enter clientId:');
-  const clientSecret = await consoleIO.query('Enter clientSecret:');
+  // open server to listen for redirect
+  server = new Hapi.server({
+    port: 3232,
+    host: "localhost",
+  });
 
-  return authUrl = `https://todoist.com/oauth/authorize?client_id=${ clientId }&client_secret=${ clientSecret }&scope=data:read;&state=${ secretString }`;
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: async (request, h) => {
+      try {
+        assert(
+          request.query.state === secretString,
+          "Looks like someone has compromised this auth flow. Quit and " +
+            "try to find a more secure network."
+        );
+
+        const accessTokenRes = await axios({
+          method: "POST",
+          url: "https://todoist.com/oauth/access_token",
+          params: {
+            client_id: clientId,
+            client_secret: clientSecret,
+            code: request.query.code,
+          },
+        });
+
+        const token = accessTokenRes.data.access_token;
+
+        console.log(
+          "\nYou did it! The response in the browser includes an access token " +
+            "for your integrations instance to use. You can go back to the " +
+            "README now to see what to do with it."
+        );
+
+        return `<h3>Your access token is <code>${token}</code></h3>`;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  });
+
+  await server.start();
+
+  console.log(
+    "\nHi, 👋. To perform the OAuth flow, first enter your apps info: "
+  );
+
+  clientId = await consoleIO.query("\nEnter your client id: ");
+  clientSecret = await consoleIO.query("\nEnter your client secret: ");
+
+  const authUrl = `https://todoist.com/oauth/authorize?client_id=${clientId}&client_secret=${clientSecret}&scope=data:read_write,data:delete&state=${secretString}`;
+
+  console.log(
+    "\nNext, visit this url in your browser and click accept. " +
+      "(if you want to)\n\n",
+    authUrl
+  );
+
+  await consoleIO.query("");
 }
 
-oAuth().then( ( url ) => {
-  console.log( url );
+oAuth().then(() => {
+  console.log("Bye 👋");
+  process.exit(0);
 });
